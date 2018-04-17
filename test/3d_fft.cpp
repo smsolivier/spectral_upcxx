1bc376782a558d0555c84abb1d5d285055d4b90b
Sam Olivier <smsolivier@gmail.com>
2018-04-17 12:19:28 -0700

    misc

--- test/3d_fft.cpp
+++ test/3d_fft.cpp
@@ -9,7 +9,8 @@
 using namespace std; 
 
 int main(int argc, char* argv[]) {
-	upcxx::init(); 
+	upcxx::init();
+	START_WTIMER();  
 	INT N = 64; 
 	INT nruns = 1; 
 	if (argc > 1) {
@@ -79,48 +80,46 @@ int main(int argc, char* argv[]) {
 	}
 
 	// write to file 
-	vector<float> x(dims[0]); 
-	vector<float> y(dims[1]); 
-	vector<float> z(Nz);
-	for (INT i=0; i<dims[0]; i++) {
-		x[i] = i*hx; 
-	} 
-	for (INT i=0; i<dims[1]; i++) {
-		y[i] = i*hy; 
-	}
-	for (INT i=0; i<Nz; i++) {
-		z[i] = (mrank*Nz + i)*hz; 
-	}
+	// vector<float> x(dims[0]); 
+	// vector<float> y(dims[1]); 
+	// vector<float> z(Nz);
+	// for (INT i=0; i<dims[0]; i++) {
+	// 	x[i] = i*hx; 
+	// } 
+	// for (INT i=0; i<dims[1]; i++) {
+	// 	y[i] = i*hy; 
+	// }
+	// for (INT i=0; i<Nz; i++) {
+	// 	z[i] = (mrank*Nz + i)*hz; 
+	// }
 
-	// ouput your data 
-	int nvars = 1; 
-	float* vals = new float[dims[0]*dims[1]*Nz]; 
-	cdouble* local = d.getLocal(); 
-	cdouble* ans_local = ans.getLocal(); 
-	for (INT i=0; i<dims[0]*dims[1]*Nz; i++) {
-		vals[i] = abs(local[i].real() - ans_local[i].real()); 
-		// vals[i] = local[i].real(); 
-	}
-	int vardim[nvars]; 
-	vardim[0] = 1; 
-	int centering[nvars]; 
-	centering[0] = 1; 
-	const char* varnames[nvars]; 
-	varnames[0] = "u"; 
+	// // ouput your data 
+	// int nvars = 1; 
+	// float* vals = new float[dims[0]*dims[1]*Nz]; 
+	// for (INT i=0; i<dims[0]*dims[1]*Nz; i++) {
+	// 	vals[i] = abs(d[i].real() - ans[i].real()); 
+	// 	// vals[i] = local[i].real(); 
+	// }
+	// int vardim[nvars]; 
+	// vardim[0] = 1; 
+	// int centering[nvars]; 
+	// centering[0] = 1; 
+	// const char* varnames[nvars]; 
+	// varnames[0] = "u"; 
 
-	string fname = "solution" + to_string(mrank); 
-	array<int,DIM> tdims = {(int)dims[0], (int)dims[1], (int)Nz}; 
-	write_rectilinear_mesh(fname.c_str(), 1, &tdims[0], &x[0], &y[0], &z[0],
-		nvars, &vardim[0], &centering[0], varnames, &vals); 
+	// string fname = "solution" + to_string(mrank); 
+	// array<int,DIM> tdims = {(int)dims[0], (int)dims[1], (int)Nz}; 
+	// write_rectilinear_mesh(fname.c_str(), 1, &tdims[0], &x[0], &y[0], &z[0],
+	// 	nvars, &vardim[0], &centering[0], varnames, &vals); 
 
-	// write master file 
-	if (mrank==0) {
-		ofstream out("solution.visit"); 
-		out << "!NBLOCKS " << upcxx::rank_n() << endl; 
-		for (int i=0; i<upcxx::rank_n(); i++) {
-			out << "solution" << i << ".vtk" << endl; 
-		}
-		out.close(); 
-	}
+	// // write master file 
+	// if (mrank==0) {
+	// 	ofstream out("solution.visit"); 
+	// 	out << "!NBLOCKS " << upcxx::rank_n() << endl; 
+	// 	for (int i=0; i<upcxx::rank_n(); i++) {
+	// 		out << "solution" << i << ".vtk" << endl; 
+	// 	}
+	// 	out.close(); 
+	// }
 	upcxx::finalize(); 
 }
